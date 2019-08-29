836447f0a8b50ae80ecafec92d8811bc84f06273
soahmkorgaokar <54686151+soahmkorgaokar@users.noreply.github.com>
2019-08-29 15:57:09 -0400

    Printing on C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Soahm Korgaokar. I love the syncing and the choice of songs we get in beat saber.");
+            Console.ReadKey();
+        }
+    }
+}
