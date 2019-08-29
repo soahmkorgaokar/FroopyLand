b03e95058073e75a0449496a15e5712707724bc9
soahmkorgaokar <54686151+soahmkorgaokar@users.noreply.github.com>
2019-08-29 16:09:00 -0400

    Changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace System
         static void Main(string[] args)
         {
             Console.WriteLine("My name is Soahm Korgaokar. I love the syncing and the choice of songs we get in beat saber.");
+            Console.Write("I hope this will");
+            Console.WriteLine(" be on the same line");
             Console.ReadKey();
         }
     }
